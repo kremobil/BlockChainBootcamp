52cdf9ca4a71557fbb7283c450f8b2daa2eee923
Wiktor Fajkowski <kremobil@wp.pl>
2024-06-25 16:22:12 +0200

    updated design and fixed some errors

--- src/first_project_frontend/src/App.vue
+++ src/first_project_frontend/src/App.vue
@@ -3,15 +3,15 @@ export default {
   data() {
     return {
       conunterIncrease: 1,
-      counter: 1
-    }
+      counter: 1,
+    };
   },
   methods: {
     increaseCounter() {
-      this.counter += this.conunterIncrease;
-    }
+      this.counter += parseInt(this.conunterIncrease);
+    },
   },
-}
+};
 </script>
 
 <template>
@@ -20,40 +20,50 @@ export default {
       {{ counter }}
     </h1>
     <div class="from_group">
-      <input v-model="counterIncrease"/>
+      <input v-model="conunterIncrease" type="number" />
       <button @click="increaseCounter">Zwiększ o podaną wartość</button>
     </div>
   </main>
 </template>
 
 <style>
-  {
-    padding: 0;
-    margin: 0;
-    color: white;
-    font-family: "Tahoma" sans-serif;
-  }
-  h1 {
-    font-size: 10rem;
-  }
-  main {
-    background: #1e1e1e;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 4rem;
-  }
-  .from_group {
-    display: flex;
-    gap: 1rem;
-  }
-  button, input {
-    padding: 1rem;
-    border: 2px solid lime;
-  }
-  button {
-    background: lime;
-    color: #1e1e1e;
-  }
+* {
+  padding: 0;
+  margin: 0;
+  color: white;
+  font-family: 'Tahoma' sans-serif;
+}
+h1 {
+  font-size: 10rem;
+}
+main {
+  background: #1e1e1e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+  height: 100vh;
+}
+.from_group {
+  display: flex;
+  gap: 1rem;
+}
+button,
+input {
+  padding: 1rem;
+  border: 2px solid lime;
+  background: transparent;
+  border-radius: 10px;
+  font-size: 24px;
+  font-weight: 700;
+}
+input {
+  color: white;
+}
+button {
+  background: lime;
+  color: #1e1e1e;
+  cursor: pointer;
+}
 </style>
